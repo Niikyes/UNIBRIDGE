15a52ad1ac4215bc4b74f6df33c7f1a7b9cdec99
Niikyes <155909575+Niikyes@users.noreply.github.com>
2025-06-21 00:22:51 -0500

    feat: :sparkles: microservice configuration
    Reading environment variables with godotenv, connecting to PostgreSQL with pgx, and using the global config.DB connection from other files

---
+++ backend/domains/student_management/practice-create-service/cmd/main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/Niikyes/UNIBRIDGE/backend/domains/student_management/practice-create-service/config"
+)
+
+func main() {
+	config.ConnectDB()
+	// Luego inicializas el router y los endpoints...
+}
