012b69cfd23ea528558b6b5a657d41075571d24e
Niikyes <155909575+Niikyes@users.noreply.github.com>
2025-06-21 21:04:29 -0500

    feat: :hammer: create data model
    defines how the JSON output will look when calling the endpoint

---
+++ backend/domains/vacancy/vacancy-get-all-service/app/models.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel
+from typing import Optional
+
+# Vacancy represents the structure of a job opening
+class Vacancy(BaseModel):
+    id: int
+    titulo: str
+    descripcion: str
+    empresa_id: int
+    ciudad: str
+    carrera: str
+    estado: str
+    fecha_publicacion: Optional[str]
