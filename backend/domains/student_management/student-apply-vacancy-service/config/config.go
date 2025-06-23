cb336434c80ac1b3b0976d6bc7dcd7c9be9dbcb3
Niikyes <155909575+Niikyes@users.noreply.github.com>
2025-06-22 21:28:41 -0500

    feat: :fire: fix bug
    add .env in file of code

--- backend/domains/student_management/student-apply-vacancy-service/config/config.go
+++ backend/domains/student_management/student-apply-vacancy-service/config/config.go
@@ -1,35 +1,38 @@
 package config
 
 import (
-    "fmt"
-    "os"
+	"fmt"
+	"os"
 
-    "github.com/joho/godotenv"
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
-    DBHost     string
-    DBPort     string
-    DBUser     string
-    DBPassword string
-    DBName     string
-    ServerPort string
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+	ServerPort string
 }
 
 func LoadConfig() (*Config, error) {
-    _ = godotenv.Load()
+	err := godotenv.Load(".env")
+	if err != nil {
+		return nil, fmt.Errorf("error cargando .env: %w", err)
+	}
 
-    return &Config{
-        DBHost:     os.Getenv("DB_HOST"),
-        DBPort:     os.Getenv("DB_PORT"),
-        DBUser:     os.Getenv("DB_USER"),
-        DBPassword: os.Getenv("DB_PASSWORD"),
-        DBName:     os.Getenv("DB_NAME"),
-        ServerPort: os.Getenv("PORT"),
-    }, nil
+	return &Config{
+		DBHost:     os.Getenv("DB_HOST"),
+		DBPort:     os.Getenv("DB_PORT"),
+		DBUser:     os.Getenv("DB_USER"),
+		DBPassword: os.Getenv("DB_PASSWORD"),
+		DBName:     os.Getenv("DB_NAME"),
+		ServerPort: os.Getenv("PORT"),
+	}, nil
 }
 
 func (c *Config) GetDBConnectionString() string {
-    return fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
-        c.DBUser, c.DBPassword, c.DBHost, c.DBPort, c.DBName)
+	return fmt.Sprintf("postgres://%s:%s@%s:%s/%s",
+		c.DBUser, c.DBPassword, c.DBHost, c.DBPort, c.DBName)
 }
